1aaf0939f453efe68b48071a9a9a332034c60cb3
Matei Teca <matei.teca@gmail.com>
2023-01-31 11:16:00 +0200

    read file cities

--- testing.js
+++ testing.js
@@ -1,10 +1,15 @@
-async function testing(){
+const fs = require('fs');
+const cities = JSON.parse(fs.readFileSync('world-cities_json.json', 'utf8'))
 
-    let response = await fetch("http://api.weatherapi.com/v1/search.json?key=a812d4795a874a76b3081357233101&q=Romania");
-    let data = await response.json();
+console.log(cities);
 
-    console.log(data);
+// async function testing(){
 
-}
+//     let response = await fetch("http://api.weatherapi.com/v1/search.json?key=a812d4795a874a76b3081357233101&q=Romania");
+//     let data = await response.json();
 
-testing();
+//     console.log(data);
+
+// }
+
+// testing();
